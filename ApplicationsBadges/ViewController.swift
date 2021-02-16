7d7c7d0a9db8c8746039a6a3523ce228ceeb1a69
Steven Joruk <sjoruk@beyondtrust.com>
2021-02-16 22:18:51 +0000

    Initial commit

---
+++ ApplicationsBadges/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ApplicationsBadges
+//
+//  Created by Steven Joruk (Standard) on 13/02/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
